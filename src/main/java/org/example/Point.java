6f089ca4c2af1316eb3e9502dccfcf97b22b1879
Mehmet KİTİŞ <103045822+mehmetkitis@users.noreply.github.com>
2024-12-23 22:54:11 +0300

    v1

---
+++ src/main/java/org/example/Point.java
@@ -0,0 +1,45 @@
+package org.example;
+
+
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance(){
+        return Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
+    }
+
+    public double distance(Point point){
+        if (point == null){
+            System.out.println("point objesi null geldi.");
+            return 0;
+        }
+        return distance(point.getX(),point.getY());
+    }
+
+    public double distance(int x, int y){
+        return Math.sqrt(Math.pow((this.x-x),2) + Math.pow((this.y-y),2));
+    }
+}
